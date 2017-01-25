309a0016e769c12680e8a8ea3dc457c10ed559f4
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-01-25 10:35:38 -0800

    push to heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    (process.env.NODE_ENV === 'production' ? 'mongodb://guest:password@ds127949.mlab.com:27949/shopping-list-mongodb' : 'mongodb://zubair.trabzada@gmail.com:start786@ds127949.mlab.com:27949/shopping-list-mongodb');
+    (process.env.NODE_ENV === 'production' ? : 'mongodb://zubair.trabzada@gmail.com:start786@ds127949.mlab.com:27949/shopping-list-mongodb');
 
 exports.PORT = process.env.PORT || 8080;
